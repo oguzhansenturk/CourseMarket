ea5bebbba281d28d19965db908c9503b607306b3
Oguzhan <oguzhansenturk@gmail.com>
2021-08-12 15:07:51 +0300

    Add project files.

---
+++ Services/Catalog/CourseMarket.Services.Catalog/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace CourseMarket.Services.Catalog.Models
+{
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ Shared/CourseMarket.Shared/Dtos/NoContent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseMarket.Shared.Dtos
+{
+    public class NoContent
+    {
+
+    }
+}
