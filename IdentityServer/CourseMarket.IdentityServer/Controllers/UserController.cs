806a2621d19c33a7bed7a332eb6a820f1b135e39
Oguzhan <oguzhansenturk@gmail.com>
2021-08-14 01:48:29 +0300

    identity server config

---
+++ IdentityServer/CourseMarket.IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using CourseMarket.IdentityServer.Dtos;
+using CourseMarket.IdentityServer.Models;
+using CourseMarket.Shared.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static IdentityServer4.IdentityServerConstants;
+
+namespace CourseMarket.IdentityServer.Controllers
+{
+    [Authorize(LocalApi.PolicyName)]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignupDto signupDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signupDto.UserName,
+                Email = signupDto.Email,
+                City = signupDto.City
+            };
+
+            var result = await _userManager.CreateAsync(user, signupDto.Password);
+
+            if (!result.Succeeded)
+                return BadRequest(Response<Shared.Dtos.NoContent>.Fail(result.Errors.Select(x => x.Description).ToList(), 400));
+
+            return NoContent();
+
+        }
+    }
+}
