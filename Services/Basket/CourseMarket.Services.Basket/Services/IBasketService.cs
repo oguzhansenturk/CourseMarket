42a8d369651700e61aee5206928213eb49162af2
Oguzhan <oguzhansenturk@gmail.com>
2021-08-15 10:07:58 +0300

    basker service add

---
+++ Services/Basket/CourseMarket.Services.Basket/Services/IBasketService.cs
@@ -0,0 +1,16 @@
+﻿using CourseMarket.Services.Basket.Dtos;
+using CourseMarket.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseMarket.Services.Basket.Services
+{
+    public interface IBasketService
+    {
+        Task<Response<BasketDto>> GetBasket(string userId);
+        Task<Response<bool>> SaveOrUpdate(BasketDto basketDto);
+        Task<Response<bool>> Delete(string userId);
+    }
+}
---
+++ Shared/CourseMarket.Shared/Services/ISharedIdentityService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseMarket.Shared.Services
+{
+    public interface ISharedIdentityService
+    {
+        public string GetUserId
+        {
+            get;
+        }
+    }
+}
