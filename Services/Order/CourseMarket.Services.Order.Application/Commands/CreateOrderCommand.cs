eaaafeddd6794e1ea79a9c40b3a04b2f62fbddbf
Oguzhan <oguzhansenturk@gmail.com>
2021-08-20 13:00:15 +0300

    add init

---
+++ Services/Order/CourseMarket.Services.Order.Application/Commands/CreateOrderCommand.cs
@@ -0,0 +1,21 @@
+﻿using CourseMarket.Services.Order.Application.Dtos;
+using CourseMarket.Shared.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseMarket.Services.Order.Application.Commands
+{
+    public class CreateOrderCommand : IRequest<Response<CreatedOrderDto>>
+    {
+        public string BuyerId { get; set; }
+
+        public List<OrderItemDto> OrderItems { get; set; }
+
+        public AddressDto AddressDto { get; set; }
+
+    }
+}
---
+++ Services/Order/CourseMarket.Services.Order.Application/Queries/GetOrdersByUserIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using CourseMarket.Services.Order.Application.Dtos;
+using CourseMarket.Shared.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseMarket.Services.Order.Application.Queries
+{
+    internal class GetOrdersByUserIdQuery : IRequest<Response<List<OrderDto>>>
+    {
+        public string UserId { get; set; }
+    }
+}
