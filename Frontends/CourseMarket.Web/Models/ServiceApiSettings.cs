70ecc66d554c70c490d2fce8c36dc06bd4a1ac3e
Oguzhan <oguzhansenturk@gmail.com>
2021-08-27 15:40:08 +0300

    web

---
+++ Frontends/CourseMarket.Web/Models/ServiceApiSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseMarket.Web.Models
+{
+    public class ServiceApiSettings
+    {
+        public string BaseUrl { get; set; }
+        public string PhotoStockUrl { get; set; }
+
+    }
+}
---
+++ Frontends/CourseMarket.Web/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,19 @@
+﻿using CourseMarket.Shared.Dtos;
+using CourseMarket.Web.Models;
+using IdentityModel.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseMarket.Web.Services.Interfaces
+{
+    interface IIdentityService
+    {
+        Task<Response<bool>> SignIn(SigninInput signinInput);
+        Task<TokenResponse> GetAccessTokenByRefreshToken();
+        Task RevokeRefreshToken();
+
+
+    }
+}
