84e8bc2785e89676d1be87a6bfad4ba44af2d623
Oguzhan <oguzhansenturk@gmail.com>
2021-10-05 13:17:12 +0300

    user service

---
+++ Frontends/CourseMarket.Web/Services/UserService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseMarket.Web.Services
+{
+    public class UserService
+    {
+    }
+}
